a7e10b1cd7e501796e1cc01c48281d8e00046ee6
g0tie <gaultierbegue@outlook.com>
2022-09-20 19:57:20 +0400

    correcting responsive

--- src/layout/Register.jsx
+++ src/layout/Register.jsx
@@ -61,6 +61,7 @@ const Register = ({}) => {
       </svg>
       Retour</span>
     </button>
+    <br />
     <Alert isVisible={isVisible}/>
     <form className="mt-8 space-y-6" action="#" method="POST">
       <input type="hidden" name="remember" value="true" />
